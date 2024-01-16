aad3a4d18c6c5d2e7c75ef4c3c1b5ad30e7ecf64
Alex Godinez <agodinez@sompo-intl.com>
2024-01-16 13:44:16 -0600

    BethanysPieShop - completed sharing state between components

--- BethanysPieShopHRM.App/BethanysPieShopHRM.App/Components/Widgets/InboxWidget.razor.cs
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Components/Widgets/InboxWidget.razor.cs
@@ -1,12 +1,18 @@
-﻿namespace BethanysPieShopHRM.App.Components.Widgets
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Components.Widgets
 {
     public partial class InboxWidget
     {
         public int MessageCount { get; set; } = 0;
 
+        [Inject]
+        public ApplicationState ApplicationState { get; set; }
+
         protected override void OnInitialized()
         {
-            MessageCount = new Random().Next(10);
+            MessageCount = ApplicationState.NumberOfMessages;
+
         }
     }
 }
