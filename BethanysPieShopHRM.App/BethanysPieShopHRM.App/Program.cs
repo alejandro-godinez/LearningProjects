4e76a23facd1d8a0fc6405d836de62b6b8363e2c
Alex Godinez <agodinez@sompo-intl.com>
2024-01-16 16:40:53 -0600

    BethanyPieShop - completed creating a form

--- BethanysPieShopHRM.App/BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Program.cs
@@ -8,8 +8,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => 
-    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddHttpClient<ICountryDataService, CountryDataService>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 builder.Services.AddScoped<ApplicationState>();
 
