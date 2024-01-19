e4315fe20905675a3a2987ac6fb638a0c3c21b44
Alex Godinez <agodinez@sompo-intl.com>
2024-01-19 13:31:20 -0600

    BlazingPizza - completed unit 7 Share data in blazor applications

---
+++ BuildWebApplicationsWithBlazor/BlazingPizza/Services/OrderState.cs
@@ -0,0 +1,44 @@
+﻿namespace BlazingPizza.Services;
+
+public class OrderState
+{
+    public bool ShowingConfigureDialog { get; private set; }
+    public Pizza ConfiguringPizza { get; private set; }
+    public Order Order { get; private set; } = new Order();
+
+    public void ShowConfigurePizzaDialog(PizzaSpecial special)
+    {
+        //initialize new configuring pizza object
+        ConfiguringPizza = new Pizza()
+        {
+            Special = special,
+            SpecialId = special.Id,
+            Size = Pizza.DefaultSize,
+            Toppings = new List<PizzaTopping>(),
+        };
+
+        //show the configure dialog
+        ShowingConfigureDialog = true;
+    }
+
+    public void CancelConfigurePizzaDialog()
+    {
+        //clear current configuring pizza
+        ConfiguringPizza = null;
+
+        //close configure dialog
+        ShowingConfigureDialog = false;
+    }
+
+    public void ConfirmConfigurePizzaDialog()
+    {
+        //add configured pizza to order pizza list
+        Order.Pizzas.Add(ConfiguringPizza);
+
+        //clear current configuring pizza
+        ConfiguringPizza = null;
+
+        //close configure dialog
+        ShowingConfigureDialog = false;
+    }
+}
