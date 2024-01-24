b3d35deba1c3d20a2524566b999d4d1b31ef001f
Alex Godinez <agodinez@sompo-intl.com>
2024-01-24 11:14:08 -0600

    GRPCClientServer - completed basic setup for client server grpc projects

---
+++ GRPCClientServer/GrpcGreeterClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+using static GrpcGreeterClient.Greeter;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7043");
+
+Console.WriteLine("Creating greeter client...");
+GreeterClient client = new GreeterClient(channel);
+Console.WriteLine("------------------");
+
+HelloRequest request = new HelloRequest { Name = "GreeterClient" };
+Console.WriteLine("Client:");
+Console.WriteLine("  " + request.ToString());
+HelloReply reply = await client.SayHelloAsync(request);
+
+Console.WriteLine("Server:");
+Console.WriteLine("  " + reply.Message);
+
+request = new HelloRequest { Name = "Alex" };
+Console.WriteLine("Client:");
+Console.WriteLine("  " + request.ToString());
+reply = await client.SayHelloAsync(request);
+
+Console.WriteLine("Server:");
+Console.WriteLine("  " + reply.Message);
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Repository for projects from learning tutorials
 - [ASP.Net Core 6 Blazor Fundamentals](https://www.pluralsight.com/courses/asp-dot-net-core-6-blazor-fundamentals)
 - [Build web applications with Blazor](https://learn.microsoft.com/en-us/training/paths/build-web-apps-with-blazor/)
 - [Build Blazor Hybrid App](https://learn.microsoft.com/en-us/training/modules/build-blazor-hybrid/)
+- [GRPC Client Server](https://learn.microsoft.com/en-us/aspnet/core/tutorials/grpc/grpc-start?view=aspnetcore-8.0&tabs=visual-studio)
